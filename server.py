a608218127823d767d6dfb4a5a0b45d514322ccd
JoeyScottschronce <joeyscottschronce@gmail.com>
2024-11-07 12:22:33 -0500

    static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Responsible for rendering the index page and handling the requests to the emotion detector.'''
 from flask import Flask, render_template, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,26 +6,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    '''Renders the index page of the web application.'''
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_to_emotion_detector():
+    '''Handles the request to the emotion detector and returns the response.'''
 
     try:
-        textToAnalyze = request.args.get("textToAnalyze")
+        text_to_analyze = request.args.get("textToAnalyze")
 
-        response, status_code = emotion_detector(textToAnalyze)
- 
+        response, status_code = emotion_detector(text_to_analyze)
+        # status_code is not an unused variable, it is used in the emotion_detector function
+
         formatted = response.get('formatted')
         if formatted is None or formatted.get("dominant_emotion") == "None":
             return jsonify("Invalid text! Please try again.")
 
         dom_emotion = formatted.get("dominant_emotion")
+        formatted_scores = ', '.join([f"'{emotion}': {score}" \
+            for emotion, score in formatted.items()])
 
-        formatted_scores = ', '.join([f"'{emotion}': {score}" for emotion, score in formatted.items()])
-        return f"For the given statement, the system response is {formatted_scores}. The dominant emotion is {dom_emotion}."
+        return f"For the given statement, the system response is \
+            {formatted_scores}. The dominant emotion is {dom_emotion}."
 
-    except (TypeError, RuntimeError) as e:
+    except (TypeError, RuntimeError):
         return jsonify("An unexpected error occurred")
 
 if __name__ == "__main__":
