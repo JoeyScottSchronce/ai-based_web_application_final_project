d4bdaf6a73a5ee3c916d0c964a5fb67ede06cd62
JoeyScottschronce <joeyscottschronce@gmail.com>
2024-11-06 21:51:29 -0500

    updated

--- static/mywebscripts.js
+++ static/mywebscripts.js
@@ -1,17 +1,12 @@
 let RunSentimentAnalysis = () => {
     let textToAnalyze = document.getElementById("textToAnalyze").value;
-    
-    if (!textToAnalyze.trim()) {
-        document.getElementById("system_response").innerHTML = "Please enter some text for analysis.";
-        return;  // Stop if no text is provided
-    }
 
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("system_response").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+encodeURIComponent(textToAnalyze), true);
+    xhttp.open("GET", "emotionDetector?textToAnalyze" + "=" + encodeURIComponent(textToAnalyze), true);
     xhttp.send();
 }
